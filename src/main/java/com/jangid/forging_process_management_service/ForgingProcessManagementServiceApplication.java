f29b7add1ddeeafad8d73a34d721f5cab69f4729
Gajendra Jangid <gjangid@Gajendras-MacBook-Pro.local>
2024-08-24 11:29:22 +0530

    SpringBoot framework files

---
+++ src/main/java/com/jangid/forging_process_management_service/ForgingProcessManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jangid.forging_process_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForgingProcessManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForgingProcessManagementServiceApplication.class, args);
+	}
+
+}
